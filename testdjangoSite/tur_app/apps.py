b237f5378a09df2cda856ab7fb39499a66684d5a
Hasan Nagiev <hasannagiev@gmail.com>
2024-12-06 13:09:17 +0400

    İlk commit

---
+++ testdjangoSite/tur_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class TurAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'tur_app'
---
+++ testdjangoSite/tur_app/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+from django.views.generic import TemplateView
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('qeydiyyat/', views.registration, name='qeydiyyat'),
+    path('about/', views.about, name='qeydiyyat'),
+
+]
+
